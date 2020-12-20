9f886792611b63a6eae955402c3709f15925d0a0
Mike M <mikester70387@gmail.com>
2020-12-19 22:57:34 -0500

    Initial Commit

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import locations from '../../data.json';
+
+import './index.css'
+
+export default function Home() {
+    return (
+        <MapContainer center={[locations.restaurants[0].latitude, locations.restaurants[0].longitude]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {
+                locations.restaurants.map(location => {
+                    console.log(location.latitude, location.longitude);
+                    return (
+                        (
+                            <Marker position={[location.latitude, location.longitude]}>
+                                <Popup>
+                                    {`${location.streetaddress}, ${location.city}, ${location.state}, ${location.country}`}
+                                </Popup>
+                            </Marker>
+                        )
+                    )
+                })
+            }
+        </MapContainer>
+    )
+}
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Home from './components/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
