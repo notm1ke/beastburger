04fdaa5dcd0cb8a88abe4a9716d2bf4603871bb8
Mike M <mikester70387@gmail.com>
2020-12-19 23:23:22 -0500

    Added more sitemeta & remove debugging

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,22 +6,19 @@ import './index.css'
 
 export default function Home() {
     return (
-        <MapContainer center={[locations.restaurants[0].latitude, locations.restaurants[0].longitude]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[locations.restaurants[0].latitude, locations.restaurants[0].longitude]} zoom={6} scrollWheelZoom={false}>
             <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | ilefa squad'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             {
                 locations.restaurants.map(location => {
-                    console.log(location.latitude, location.longitude);
                     return (
-                        (
-                            <Marker position={[location.latitude, location.longitude]}>
-                                <Popup>
-                                    {`${location.streetaddress}, ${location.city}, ${location.state}, ${location.country}`}
-                                </Popup>
-                            </Marker>
-                        )
+                        <Marker position={[location.latitude, location.longitude]}>
+                            <Popup>
+                                {`${location.streetaddress}, ${location.city}, ${location.state}, ${location.country}`}
+                            </Popup>
+                        </Marker>
                     )
                 })
             }
